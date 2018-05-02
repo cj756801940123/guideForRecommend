17665a059aaa8ad87ee06dbaf3268abdbc1fa291
Cccj <cj756801940@163.com>
2018-05-02 23:56:53 +0800

    add search keywords

--- guideForRecommend/urls.py
+++ guideForRecommend/urls.py
@@ -17,10 +17,13 @@ from django.conf.urls import url
 from django.contrib import admin
 #利用TemplateView
 from django.views.generic import TemplateView
-
+from Recommender.handler import search
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name="index-1.html"), name="index"),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name="index"),
+
+    # url(r'^Recommender/handler/search/search_product', search.search_product),
+    url(r'^search_product', search.search_product),
 ]
