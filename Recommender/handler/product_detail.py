e3894f44f88348e7075d32b77932abfc66182fbd
Cccj <cj756801940@163.com>
2018-05-10 18:05:09 +0800

    设计商品详情界面的评论部分

--- Recommender/handler/product_detail.py
+++ Recommender/handler/product_detail.py
@@ -12,6 +12,7 @@ import jieba
 import json
 
 FILE_PATH = (os.path.dirname(os.path.abspath("search.py")) + '/Recommender/data/').replace('\\','/')
+DATA_PATH = (os.path.dirname(os.path.dirname(os.path.abspath("search.py"))) + '/RecommendData/').replace('\\','/')
 
 def get_product_info(table,sku):
     #数据初始化
@@ -38,11 +39,39 @@ def get_product_info(table,sku):
         result["avg_price"] = temp[10]
     return result
 
+def get_comment(table,sku):
+    useful_file = DATA_PATH+table+'/score_comments/'+sku+'.txt'
+    try:
+        file = open(useful_file, "r", encoding='utf-8')
+        useful_comments = []
+        for each_line in file:
+            temp = {}
+            index = each_line.index(' ')
+            score = each_line[0:index]
+            star = each_line[index+1:index+2]
+            c_index = each_line.find(' comment:')
+            nickname = each_line[index+12:c_index]
+            commnet = each_line[c_index+9:].strip('\n')
+            temp['score'] = score
+            temp['star'] = star
+            temp['nickname'] = nickname
+            temp['comment'] = commnet
+            useful_comments.append(temp)
+
+
+    except Exception as err:
+        print('product_detail get_comment err:'+str(err))
+    finally:
+        file.close()
+
+    return useful_comments
+
 
 @require_http_methods(["POST"])
 def get_product_detail(request):
     table = 'cellphone'
     sku = request.POST.get("sku", '')
     result = get_product_info(table,sku)
-    return render(request, "product-detail.html",{'result':result})
+    score_comments = get_comment(table, sku)
+    return render(request, "product-detail.html",{'result':result,'score_comments':score_comments})
 
